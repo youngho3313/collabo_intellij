2219362c4b325fdd9ea121e17d0730ceec6952d5
youngho3313 <youngho3313@gmail.com>
2023-09-22 10:15:30 +0900

    2개의 파일이 수정 되었습니다.

--- src/ch01/Hello.java
+++ src/ch01/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hollo01");
         System.out.println("hollo02");
+        System.out.println("hollo03");
     }
 }
--- src/ch01/World.java
+++ src/ch01/World.java
@@ -3,5 +3,6 @@ package ch01;
 public class World {
     public static void main(String[] args) {
         System.out.println("world01");
+        System.out.println("world02");
     }
 }
